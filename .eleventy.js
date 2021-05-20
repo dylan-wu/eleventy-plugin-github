f4235b1c4fb281c50334f3cfa0c299d0b7b7b72d
Gary Wu <dylanwu@live.com>
2021-05-19 21:18:21 -0500

    add caching

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,40 @@
 const { Octokit, App, Action } = require("octokit");
+const {AssetCache} = require("@11ty/eleventy-cache-assets");
+const debug = require("debug")("EleventyGithub");
 
 const getIssues = async ({ key, owner, repo }) => {
+  let asset = new AssetCache(`${owner}_${repo}`, '.');
+
+  let issues = null
+
+  if(asset.isCacheValid("1d")) {
+    issues = await asset.getCachedValue()
+    debug("from cache");
+  } else {
     const octokit = new Octokit({ auth: key});
-    
     let response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-        owner: owner,
-        repo: repo
+      owner: owner,
+      repo: repo
     })
-
-    let issues = response.data
-    for (const issue of issues){
+  
+    issues = response.data
+  
+    if (issues.comments){
+      for (const issue of issues){
         const response = await octokit.request('GET /repos/{owner}/{repo}/issues/{number}/comments', {
-            owner: owner,
-            repo: repo,
-            number: issue.number
-          })
-
+          owner: owner,
+          repo: repo,
+          number: issue.number
+        })
+  
         issue.comments = response.data
+      }
     }
-    return issues
+  
+    await asset.save(issues, "json");
+    debug("from remote");
+  }
+  return issues
 };
 
 const getContent = async (params) => {
